9401b03ea20f730d58de5ad90bdad6df8ca79691
Darshan Sharma <thedarshansharma@gmail.com>
2017-10-22 11:32:32 +0530

    added chrome and other pkgs installation

--- install_prog.sh
+++ install_prog.sh
@@ -5,12 +5,19 @@ clear
 echo "Hi, $USER!"
 
 os_name=$(cat /etc/*release | grep ^NAME | tr -d 'NAME="')
-$plist = "sudo apt install vim; sudo apt install shutter;"
+ins_deb="sudo dpkg -i "
+py_pkgs="pip install --user requests; pip install --user bs4; pip install --user numpy"
+ins_cmds="sudo apt update; sudo apt install vim; sudo apt install shutter; sudo apt install python-pip; sudo apt install weka; sudo apt install screenfetch; sudo apt install "
 if [ "$os_name" = Ubuntu ]
 then
-	echo "OS ubuntu detected."
+	echo "ubuntu detected."
 	echo "Lets begin the installation"
-	eval $plist
-	
+	echo "Starting with Chrome"
+	echo "\n"
+	eval "wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+	eval "sudo dpkg -i google-chrome-stable_current_amd64.deb; sudo apt install -f"
+	echo -e "\033[0;32m Chrome Successfully Installed\e[m"
+	echo "\n"
+	eval $ins_cmds
 fi
 
